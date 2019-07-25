0657d28a2801cc71f6c9839096163af8f85e88ae
hcymysql <chunyang_he@139.com>
2019-07-25 11:28:40 +0800

    Update mgr_Check_primary_class.php

--- mgr_Check_primary_class.php
+++ mgr_Check_primary_class.php
@@ -52,7 +52,7 @@ class Check_primary {
 					} else {
 						file_put_contents(dirname(__FILE__)."/".strstr($GLOBALS['filename'],'.',true)."_master_status.health", date('Y-m-d H:i:s')."\n\n数据延迟 ".$row2[0]." 个事务，不能切换VIP"."\n\n", FILE_APPEND);
 						echo "\e[38;5;196m数据延迟 ".$row2[0]." 个事务，不能切换VIP".PHP_EOL;
-						exit;
+						break;
 					}
 					return $c=1;
 				} else {
