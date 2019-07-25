af2592d47b72718b496229ff9899f8905789103a
hcymysql <chunyang_he@139.com>
2019-07-25 18:02:44 +0800

    Update mgr_configure1.php

--- mgr_configure1.php
+++ mgr_configure1.php
@@ -3,12 +3,17 @@
 //mgr Remote Primary connection information
 $primary_ip='192.168.148.41';
 $new_primary_ip='192.168.148.42';
+
 $user='admin';
-$passwd='159753';
+$passwd='123456';
+
 $port='3306';
 $new_port='3307';
+
 $ssh_port='60000';
+
 $vip='192.168.148.43';
+
 $network_name='eth0';
 
 //mgr hostname
